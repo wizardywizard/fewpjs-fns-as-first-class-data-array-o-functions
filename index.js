6c6b8b245b19acde72b2e40e8a16049f1f92f953
wizardywizard <littledavid175@gmail.com>
2020-02-08 14:36:31 -0800

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function independentFunctions(name, breed){
+    function wakeDog(name, breed){
+        console.log(`Wake ${name} the ${breed}`);
+    }
+    // function leashDog(name, breed){
+    //     console.log(`Leash ${name} the ${breed}`)
+    // }
+}
