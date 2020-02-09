274df37cb84a9f90dd6892e043c6936e246bc0eb
wizardywizard <littledavid175@gmail.com>
2020-02-08 16:22:18 -0800

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-function independentFunctions(name, breed){
     function wakeDog(name, breed){
         console.log(`Wake ${name} the ${breed}`);
+        return `Wake ${name} the ${breed}`
     }
-    // function leashDog(name, breed){
-    //     console.log(`Leash ${name} the ${breed}`)
-    // }
-}
+    function leashDog(name, breed){
+        console.log(`Leash ${name} the ${breed}`);
+        return `Leash ${name} the ${breed}`
+    }
+    function walkToPark (name, breed){
+        console.log(`Walk to the park with ${name} the ${breed}`);
+        return `Walk to the park with ${name} the ${breed}`
+    }
+    function throwFrisbee(name, breed){
+        console.log(`Throw the frisbee for ${name} the ${breed}`);
+        return `Throw the frisbee for ${name} the ${breed}`
+    }
+    function walkHome(name, breed){
+        console.log(`Walk home with ${name} the ${breed}`);
+        return `Walk home with ${name} the ${breed}`
+    }
+    function unleashDog(name, breed){
+        console.log(`Unleash ${name} the ${breed}`);
+        return `Unleash ${name} the ${breed}`
+    }
+
+    var routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+    function exerciseDog(name, breed){        
+         return routine.map(dog =>{
+           return dog(name, breed)
+          })
+        }
+
